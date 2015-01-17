f57e97bd9d13a6f4428c2fbe5bd2e16f3ca1625f
varsha <varsha@kth.se>
2015-01-17 03:00:29 +0100

    Analysing variation in the data

---
+++ dataPlot.m
@@ -0,0 +1,16 @@
+pos = dlmread('posFileData');
+neg = dlmread('negFileData');
+B = pos > 0;
+posLen = sum(B,2);
+B = neg > 0;
+negLen = sum(B,2);
+
+for i = 1:100
+    d   = randi(1320);
+figure;
+plot(pos(d,:)','.-b');
+hold on;
+plot(neg(d,:)','.-r');
+pause;
+close;
+end
